c28d248bea5c6f54ecf2ae00384e0951b01e0d04
Robert Emil Berge <post@robertemilberge.no>
2022-09-09 02:33:26 +0200

    Set version 0.2.

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -89,7 +89,7 @@
     </section>
 
     <footer class="grid-bottom">
-      <p>A New Methodology and Ethics for Manuscript Studies (EthiCodex)<span style="float: right">v0.1</span></p>
+      <p>A New Methodology and Ethics for Manuscript Studies (EthiCodex)<span style="float: right">v0.2</span></p>
     </footer>
   </div>
 </template>
