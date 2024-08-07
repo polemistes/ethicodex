e7eb143ad8b34325991472b6513e57f1c0a0531f
Robert Emil Berge <post@robertemilberge.no>
2024-08-07 03:47:49 +0200

    Revert failed removal of purchase_id field.

--- database/migrations/2024_08_06_003950_add_columns_to_documents_table.php
+++ database/migrations/2024_08_06_003950_add_columns_to_documents_table.php
@@ -14,8 +14,6 @@ return new class extends Migration
     public function up()
     {
         Schema::table('documents', function (Blueprint $table) {
-            $table->dropForeign(['purchase_id']);
-            $table->dropColumn('purchase_id');
             $table->foreignId('gregorys_rule_id')->nullable()->after('quire_comment');
             $table->text('gregorys_rule_comment')->nullable()->after('gregorys_rule_id');
             $table->smallInteger('columns')->nullable()->after('gregorys_rule_comment');
